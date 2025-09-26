049670be14cfb3472220ee6666b989a3a90419eb
arnaucube <git@arnaucube.com>
2025-09-26 13:11:35 +0200

    modify code to be cli friendly, to load the r1cs & trusted setup instead of regenerating it each time (#3)

--- full-flow.sh
+++ full-flow.sh
@@ -5,8 +5,12 @@ echo "- [generating plonky2 proof]: cargo test --release test_pod_flow -- --noca
 cargo test --release test_pod_flow -- --nocapture
 
 echo -e "\n================================================================"
-echo "- [generating groth16 proof & solidity contract]: go run main.go"
-go run main.go
+echo "- [generating groth16 trusted setup]: go run main.go -t"
+go run main.go -t
+
+echo -e "\n================================================================"
+echo "- [generating groth16 proof & solidity contract]: go run main.go -p -s"
+go run main.go -p -s
 
 echo -e "\n================================================================"
 echo "- proof, verifying key and the Solidity smart contract to verify the proofs can be found at 'outputs' directory"
